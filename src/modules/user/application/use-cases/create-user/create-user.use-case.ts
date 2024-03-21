2f81ce0ba49c341f47b2b084384ae239a1f6e7fa
luizphelip3 <luiz_phelip3@hotmail.com>
2024-03-21 17:22:00 -0300

    feat: added bcrypt

--- src/modules/user/application/use-cases/create-user/create-user.use-case.ts
+++ src/modules/user/application/use-cases/create-user/create-user.use-case.ts
@@ -1,7 +1,8 @@
+import { UserTypeOrmRepository } from '@modules/user/infra/repository/user.repository';
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 import { User } from '../../../domain/entity/user.entity';
 import { CreateUserDTO } from './dto/create-user.dto';
-import { UserTypeOrmRepository } from '@modules/user/infra/repository/user.repository';
 
 @Injectable()
 export class CreateUserUseCase {
@@ -9,6 +10,7 @@ export class CreateUserUseCase {
 
   async execute(input: CreateUserDTO) {
     const userToCreate = new User(input);
+    userToCreate.password = await bcrypt.hash(userToCreate.password, 10);
     const createdUser = await this.userRepository.create(userToCreate);
 
     return createdUser;

bc53ecf92f7ec0934e5843b1b8e9555a44d342dd
luizphelip3 <luiz_phelip3@hotmail.com>
2024-03-21 17:35:24 -0300

    fix: fixing bcrypt hash method import

--- src/modules/user/application/use-cases/create-user/create-user.use-case.ts
+++ src/modules/user/application/use-cases/create-user/create-user.use-case.ts
@@ -1,8 +1,8 @@
 import { UserTypeOrmRepository } from '@modules/user/infra/repository/user.repository';
 import { Injectable } from '@nestjs/common';
-import * as bcrypt from 'bcrypt';
 import { User } from '../../../domain/entity/user.entity';
 import { CreateUserDTO } from './dto/create-user.dto';
+import { hash } from 'bcrypt';
 
 @Injectable()
 export class CreateUserUseCase {
@@ -10,7 +10,7 @@ export class CreateUserUseCase {
 
   async execute(input: CreateUserDTO) {
     const userToCreate = new User(input);
-    userToCreate.password = await bcrypt.hash(userToCreate.password, 10);
+    userToCreate.password = await hash(userToCreate.password, 10);
     const createdUser = await this.userRepository.create(userToCreate);
 
     return createdUser;
